6b959613f2aa42e3c92a5a34978b6aace8b4b3fc
loading-torrent-9632 <angelrey@buffalo.edu>
2019-04-12 20:29:02 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,6 @@
 def square_array(array)
-  new_array = []
-  array.each do |number|
-    new_array << number * number
-  end
-  return new_array
+  # new_array = []
+  # array.each { |number| new_array << number * number }
+  # return new_array
+  array.collect { |number| number * number } 
 end
